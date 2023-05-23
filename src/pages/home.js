79096b08023c55111444236a79161b6766c71c53
realvalkry <122685848+realvalkry@users.noreply.github.com>
2023-05-23 10:31:57 -0500

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ const Home = () => {
             <img class="set-width-logo" src="./Valkry-large.jpg" alt="" />
           </div>
           <div class="col-12 action-collect">
-            <button class="btn btn-primary">MINT MONGRELS</button>
+            <button class="btn btn-primary"><a href="https://saturnnft.io/mint/e3cf323e-e535-47fc-afb2-3313595f5b03">MINT MONGRELS</a></button>
             <button class="btn btn-primary">MY DASHBOARD</button>
             <button class="btn btn-primary">THE COLLECTION</button>
             <button class="btn btn-primary">CLUBS / PACKS</button>
