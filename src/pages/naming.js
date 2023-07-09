f215b8422a0cf93b0535f35735563da871d8494f
gliterpoint <freefake476@gmail.com>
2023-07-09 23:56:50 +0500

    Added Form

--- src/pages/naming.js
+++ src/pages/naming.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -6,6 +7,40 @@ const Naming = () => {
   const navigateMongerels = () => {
     navigate("/the-collection");
   };
+
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      id,
+      name,
+      email,
+    };
+
+    fetch("https://rehmankabir.com/api/sendEmail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        alert(data.message);
+        // Handle success or display error message to the user
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Handle error or display error message to the user
+      });
+  };
+
   return (
     <>
       <div class="container">
@@ -14,19 +49,68 @@ const Naming = () => {
             <img class="mongerels" src="./mongerels.jpg" alt="" />
           </div>
 
-          <div class="col-12 text-center">
+          {/* <div class="col-12 text-center">
             <p>
               Naming
             </p>
             <p>
               This is how you name your mongrel.
             </p>
+          </div> */}
+
+          <div className="form-container-new">
+            <h1>Get in Touch</h1>
+            <form>
+              <div className="form-control-field">
+                <label htmlFor="mongrelID">Mongreal ID</label>
+                <input
+                  type="text"
+                  placeholder="Mongrel ID"
+                  id="ongrelID"
+                  required
+                  value={id}
+                  onChange={(e) => setId(e.target.value)}
+                />
+              </div>
+              <div className="form-control-field">
+                <label htmlFor="username">Name</label>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  id="username"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="form-control-field">
+                <label htmlFor="email">Email Address</label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  id="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-control-checkbox">
+                <input
+                  type="checkbox"
+                  id="agreement"
+                  checked={isChecked}
+                  onChange={(e) => setIsChecked(e.target.checked)}
+                />
+                <label htmlFor="agreement">
+                  I agree to the <a href="#"> terms and conditions </a>
+                </label>
+              </div>
+              <button type="submit" className="submit-btn" onClick={handleSubmit}>
+                Submit
+              </button>
+            </form>
           </div>
-         
         </div>
-       
-       
-       
       </div>
     </>
   );
